3ec180dab0116c01194c424582145514b96ebb18
aman <amannema34@gmail.com>
2024-02-01 15:09:22 +0530

    Remove port number to env

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
-app.listen(port, () => {
+app.listen(5000, () => {
   console.log(`Server running on port ${port}`);
 });
